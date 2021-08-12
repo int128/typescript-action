406ced47fe1614fa8d8cd211b5531f6900ba3c3c
Hidetake Iwata <int128@gmail.com>
2021-08-12 09:47:47 +0900

    Remove eslint-plugin-github

--- src/run.ts
+++ src/run.ts
@@ -4,6 +4,7 @@ type Inputs = {
   name: string
 }
 
+// eslint-disable-next-line @typescript-eslint/require-await
 export const run = async (inputs: Inputs): Promise<void> => {
   core.info(`my name is ${inputs.name}`)
 }
