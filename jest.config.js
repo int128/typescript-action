42968f3f150a371d093042f9a893a202eb366bad
Hidetake Iwata <int128@gmail.com>
2024-03-17 11:58:34 +0900

    Fix

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,8 @@
 export default {
   preset: 'ts-jest/presets/default-esm',
   clearMocks: true,
+  // https://kulshekhar.github.io/ts-jest/docs/guides/esm-support/
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
 }
