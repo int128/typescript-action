a30e3d879ede7eb0429358d1ee35931ce76916e1
Hidetake Iwata <int128@gmail.com>
2024-04-06 21:08:05 +0900

    Fix jest

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,16 @@
 // @ts-check
 
 import eslint from '@eslint/js'
+import jest from 'eslint-plugin-jest'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-  { ignores: ['.git', 'node_modules', 'dist', 'eslint.config.js'] },
+  {
+    ignores: ['.git', 'node_modules', 'dist', 'eslint.config.js'],
+  },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
+  jest.configs['flat/recommended'],
   {
     languageOptions: {
       parserOptions: {
