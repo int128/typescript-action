9f102c1eda3ff9672b1a207d3427010e8d6a3394
Hidetake Iwata <int128@gmail.com>
2024-04-21 16:12:17 +0900

    Ignore config files in eslint

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
   {
-    ignores: ['.git/', 'node_modules/', 'dist/', 'eslint.config.js'],
+    ignores: ['.git/', 'node_modules/', 'dist/', '*.config.*'],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
