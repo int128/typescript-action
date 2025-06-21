fb7fe5f5990fea2879c79a29dac07b7f7529af33
Hidetake Iwata <int128@gmail.com>
2025-06-21 11:20:00 +0900

    Add Octokit to template

--- src/run.ts
+++ src/run.ts
@@ -1,10 +1,20 @@
 import * as core from '@actions/core'
+import { Context } from './github.js'
+import { Octokit } from '@octokit/action'
 
 type Inputs = {
   name: string
 }
 
-// eslint-disable-next-line @typescript-eslint/require-await
-export const run = async (inputs: Inputs): Promise<void> => {
-  core.info(`my name is ${inputs.name}`)
+export const run = async (inputs: Inputs, octokit: Octokit, context: Context): Promise<void> => {
+  core.info(`The name is ${inputs.name}`)
+
+  const { data: pulls } = await octokit.rest.repos.listPullRequestsAssociatedWithCommit({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    commit_sha: context.sha,
+  })
+  for (const pull of pulls) {
+    core.info(`Associated pull request: ${pull.html_url}`)
+  }
 }
